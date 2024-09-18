a3f4e5c69a61809e553037afb3906dcd8c714489
Yusuf <ygazi465@gmail.com>
2024-09-18 13:29:47 +0530

    com

--- src/App.js
+++ src/App.js
@@ -32,6 +32,43 @@
 
 // export default App;
 
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import Features from './components/Features';
+// import Footer from './components/Footer';
+// import About from './components/About';
+// import Contact from './components/Contact';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Routes>
+//           <Router basename={process.env.PUBLIC_URL}>
+//             <switch>
+//             <Route path="/" element={
+//             <>
+//               <Hero />
+//               <Features />
+//             </>
+//           } />
+//             </switch>
+//           </Router>
+         
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -43,17 +80,22 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Navbar />
         <Routes>
+          {/* Homepage Route */}
           <Route path="/" element={
             <>
               <Hero />
               <Features />
             </>
           } />
+
+          {/* About Page Route */}
           <Route path="/about" element={<About />} />
+
+          {/* Contact Page Route */}
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
